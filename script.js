2643a91a66138ca467bfe18fe6ed131389427db5
Aashni Shah <contact@aashni.me>
2016-03-09 22:11:33 -0500

    separating quotes into their own file

--- script.js
+++ script.js
@@ -4,19 +4,6 @@ var attr_opacity = 1;
 var lastScrollPos = 0;
 var initialTop = window.pageYOffset || document.documentElement.scrollTop;
 var selectedQuote = -1;
-var quotes = [
-		'"Imperfection is beauty, madness is genius and it\'s better to be absolutely ridiculous than absolutely boring." - Marilyn Monroe',
-		'"There\'s a big, wonderful world out there for you. It belongs to you. It\'s exciting and stimulating and rewarding. Don\'t cheat yourselves out of this promise." - Nancy Reagan',
-		'"There is no greater gift you can give or receive than to honor your calling. It\'s why you were born." - Oprah Winfrey',
-		'"Be a first-rate version of yourself, instead of a second-rate version of somebody else." - Judy Garland',
-		'"You can waste your lives drawing lines. Or you can live your life crossing them." - Shonda Rhimes',
-		'"A woman is like a tea bag; you never know how strong it is until it\'s in hot water." - Eleanor Roosevelt',
-		'"I hope the fathers and mothers of little girls will look at them and say \'yes, women can.\'" - Dilma Rousseff',
-		'"Above all, be the heroine of your life, not the victim." - Nora Ephron',
-		'"You are the one that possesses the keys to your being. You carry the passport to your own happiness." - Diane von Furstenberg',
-		'I know for sure that what we dwell on is what we become. - Oprah Winfrey',
-		'"I just try and surround myself, for the biggest proportion of time that I can, with people who make me feel normal, because constantly feeling abnormal is quite difficult." - Emma Watson'
-	];
 
 function parallex() {
 	// scroll direction
